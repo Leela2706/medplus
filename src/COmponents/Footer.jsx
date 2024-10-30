7f0b8f0b3dad0345eb1c065f81037ed5e30ecfe0
leela2706 <saileelavardhan12@gmail.com>
2024-10-30 23:13:50 +0530

    added

--- src/COmponents/Footer.jsx
+++ src/COmponents/Footer.jsx
@@ -53,7 +53,7 @@ export default class Footer extends Component {
           <div className="col-lg-3 col-md-6 col-12">
             <div className="single-footer">
               <h2>Open Hours</h2>
-              <p>Lorem ipsum dolor sit ame consectetur adipisicing elit do eiusmod tempor incididunt.</p>
+              <p>Lorem ipsum dolor sit ame consectetur adipisicing elit do eiusmod tempor incididunt consectetur adipisicing .</p>
               <ul className="time-sidual">
                 <li className="day">Monday - Fridayp <span>8.00-20.00</span></li>
                 <li className="day">Saturday <span>9.00-18.30</span></li>
